9af5f520b6bce79cecf68267f8f0ff01b2918377
CrAzD <jrobichaud@outlook.com>
2017-07-26 23:16:31 -0400

    0

--- game/dota_addons/vscripts/libraries/TakingManager/Setup.lua
+++ game/dota_addons/vscripts/libraries/TakingManager/Setup.lua
@@ -6,7 +6,7 @@ require('libraries/TakingManager/Manager')
 local manager = TakingManagerInitialization(class({}))
 
 -- Information
-manager['version'] = 0.09
+manager['version'] = 0.10
 manager['url'] = 'https://github.com/CrAzD/DotaTakingManager'
 manager['emMinimumVersion'] = 0.64
 
@@ -20,16 +20,21 @@ manager['kv'] = {
     ['takers'] = LoadKeyValues('scripts/kv/tm_takers.kv') or {}
 }
 
--- Check if EM has been iniltialized
-if not EntityManager or not EntityManager['initialized'] then
-	print('TakingManager: EntityManager required but missing. Please download and require before TakingManager.')
-	return
+-- Check dependency initialization and minimum version met.
+if EntityManager and EntityManager['initialized'] then
+    if EntityManager['version'] < manager['emMinimumVersion'] then
+        print('BuildingManager: DEPENDENCY VERSION NOT MET!'..
+            '\n\tEntityManager must be at least verison-'..tostring(manager['emMinimumVersion'])..' to be compatible with BuildingManager.'
+        )
+        return(nil)
+    end
+else
+    print('BuildingManager: DEPENDENCY NOT LOADED!'..
+        '\n\tEntityManager version-'..tostring(manager['emMinimumVersion'])..' (or above) missing or not loaded before BuildingManager.'
+    )
+    return(nil)
 end
--- Check if EM's version meets the minimum required for TM
-if EntityManager['version'] < manager['emMinimumVersion'] then
-	print('TakingManager: EntityManager below minimum required verison. \n\tPlease update EntityManager to at least version '..manager['emMinimumVersion'])
-	return
-end
+
 -- Check if TM has been iniltialized
     -- If not iniltialize
 if not manager['initialized'] then
